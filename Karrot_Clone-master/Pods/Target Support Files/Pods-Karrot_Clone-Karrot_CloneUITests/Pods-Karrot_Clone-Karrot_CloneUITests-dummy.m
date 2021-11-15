fde84b4651bf9e3bc2847534a87f85d6c9ac5982
leeyunsik <leeys9932@naver.com>
2021-11-15 23:57:01 +0900

    ios

--- Karrot_Clone-master/Pods/Target Support Files/Pods-Karrot_Clone-Karrot_CloneUITests/Pods-Karrot_Clone-Karrot_CloneUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Karrot_Clone_Karrot_CloneUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Karrot_Clone_Karrot_CloneUITests
-@end
--- Karrot_Clone-master/Pods/Target Support Files/Pods-Karrot_CloneTests/Pods-Karrot_CloneTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Karrot_CloneTests : NSObject
-@end
-@implementation PodsDummy_Pods_Karrot_CloneTests
-@end

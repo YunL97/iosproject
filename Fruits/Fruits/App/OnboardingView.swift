f2632e5207a8bd42ada6a48d18fa4691c9567eed
leeyunsik <leeys9932@naver.com>
2021-12-04 23:36:44 +0900

    ios

--- Fruits/Fruits/App/OnboardingView.swift
+++ Fruits/Fruits/App/OnboardingView.swift
@@ -9,12 +9,17 @@ import SwiftUI
 
 struct OnboardingView: View {
     // MARK: - PROPERTIES
-    
+    var fruits: [Fruit] = fruitsData
+
     // MARK: - BODY
     var body: some View {
         TabView {
-            FruitCardView()
+            ForEach(fruits[0...5]) {item in
+                FruitCardView(fruit: item)
+            }
         }
+        .tabViewStyle(PageTabViewStyle())
+        .padding(.vertical, 20)
     }
     
     
@@ -24,6 +29,6 @@ struct OnboardingView: View {
 
 struct OnboardingView_Previews: PreviewProvider {
     static var previews: some View {
-        OnboardingView()
+        OnboardingView(fruits: fruitsData)
     }
 }
--- Fruits/Fruits/New Groupㅡㅐㅇ디/FruitModel.swift
+++ Fruits/Fruits/New Groupㅡㅐㅇ디/FruitModel.swift
@@ -5,4 +5,17 @@
 //  Created by 이윤식 on 2021/12/03.
 //
 
-import Foundation
+//import Foundation
+import SwiftUI
+
+// MARK: - FRUIT DATA MODEL
+
+struct Fruit: Identifiable {
+    var id = UUID() //유일키 생성
+    var title : String
+    var headline : String
+    var image: String
+    var gradientColors: [Color]
+    var description: String
+    var nutrition: [String]
+}

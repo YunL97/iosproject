bb61f23c03cce758d3b66d676476530feaa09809
YunL97 <leeys9932@naver.com>
2021-12-23 22:24:09 +0900

    removw

--- UIKit/PassData/PassData/NotiDetailViewController.swift
+++ UIKit/PassData/PassData/NotiDetailViewController.swift
@@ -4,7 +4,7 @@
 //
 //  Created by 이윤식 on 2021/12/17.
 //
-
+//
 import UIKit
 
 class NotiDetailViewController: UIViewController {
@@ -28,7 +28,7 @@ class NotiDetailViewController: UIViewController {
     /*
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    // In a  storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.

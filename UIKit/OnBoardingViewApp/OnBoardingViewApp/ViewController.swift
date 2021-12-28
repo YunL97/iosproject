6ef5bb4a496e8435706f46cca54a5dc4f73049d7
YunL97 <leeys9932@naver.com>
2021-12-28 19:30:23 +0900

    main

--- UIKit/OnBoardingViewApp/OnBoardingViewApp/ViewController.swift
+++ UIKit/OnBoardingViewApp/OnBoardingViewApp/ViewController.swift
@@ -9,11 +9,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    
+    
+
+    
+
+   
+    //var didShowOnboardingView = false
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        if UserDefaults.standard.object(forKey: "ff") == nil {
+            UserDefaults.standard.set(false, forKey: "ff")
+            
+        }
     }
-
-
+       
+    
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+//        super.viewDidAppear(animated)
+            //didShowOnboardingView == false
+        
+   
+        
+        var user = UserDefaults.standard.bool(forKey: "ff")
+        print(user)
+        if user == false {
+        let pageVC = OnBoardingPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal, options: .none)
+            
+        //전체화면
+        pageVC.modalPresentationStyle = .fullScreen
+        self.present(pageVC, animated: true, completion: nil)
+//            didShowOnboardingView = true
+            
+        }
 }
+}
+    
 

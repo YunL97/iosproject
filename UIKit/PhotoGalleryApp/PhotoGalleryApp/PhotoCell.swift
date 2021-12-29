cc3b89d47953856baadbcbe2bfc6a999f0b81e8a
YunL97 <leeys9932@naver.com>
2021-12-29 21:38:31 +0900

    cell xib없이 설정

---
+++ UIKit/PhotoGalleryApp/PhotoGalleryApp/PhotoCell.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoCell.swift
+//  PhotoGalleryApp
+//
+//  Created by 이윤식 on 2021/12/29.
+//
+
+import UIKit
+
+class PhotoCell : UICollectionViewCell {
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+}

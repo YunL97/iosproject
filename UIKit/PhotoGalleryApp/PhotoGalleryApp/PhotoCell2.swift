6859710cb89af78066f30b3c38c295b21cdbd26e
YunL97 <leeys9932@naver.com>
2021-12-30 19:14:03 +0900

    photoGallery

---
+++ UIKit/PhotoGalleryApp/PhotoGalleryApp/PhotoCell2.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoCell2.swift
+//  PhotoGalleryApp
+//
+//  Created by 이윤식 on 2021/12/29.
+//
+
+import UIKit
+
+class PhotoCell2: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}

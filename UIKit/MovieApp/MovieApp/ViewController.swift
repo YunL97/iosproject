fa37d138cee8c0d8ac5f04b89ff17ee6f0f2ed2d
YunL97 <leeys9932@naver.com>
2022-01-01 22:47:31 +0900

    이미지, date 변환 추가

--- UIKit/MovieApp/MovieApp/ViewController.swift
+++ UIKit/MovieApp/MovieApp/ViewController.swift
@@ -22,8 +22,45 @@ class ViewController: UIViewController{
         movieTableView.dataSource = self
         searchBar.delegate = self
         requestMovieAPI()
+        
+        
     }
  
+    //이미지 값이 아닌 이미지가 들어있는 경로가 들어있기 떄문에 한번더 decode 해줘야함
+    func loadImage(urlString: String, completion: @escaping (UIImage?) -> Void){
+        let sessionConfig = URLSessionConfiguration.default
+        let session = URLSession(configuration: sessionConfig)
+        
+        if let hasURL = URL(string: urlString){
+            
+            var request = URLRequest(url: hasURL)
+            request.httpMethod = "GET"
+            
+            //데이터 get 요청 해서 response로 받아온다
+            session.dataTask(with: request) { data, response, error in
+               
+                //상태코드 ex: 200, 404 *****
+                print((response as! HTTPURLResponse).statusCode)
+                
+                
+                //여기서 탈출클로저 선언해주는 이유 @escaping 이유는
+                //클로저 안의 data 는 이 클로저 안에서만 있고 나가면 없어진다
+                //그런데 이loadImage 함수를 호출 했을 때 data를 밖에서도 사용하고
+                //싶기 때문에 @escaping을 사용한다
+                if let hasData = data {
+                    DispatchQueue.main.async {
+                        //hadData를 통해서 data가 밖으로 나가기 때문에 @escaping 해줘야함
+                        completion(UIImage(data: hasData))
+                    }
+                
+                return
+                }
+            }.resume()
+            session.finishTasksAndInvalidate()
+        }
+        //살패 했을때도 메모리를 계속 가지고 있기 때문에 실패했을때 nil을 넣어줘서 메모리를 해제 해줘야한다
+        completion(nil)
+    }
 
     //nework, json 데이터 받아옴
     func requestMovieAPI(){
@@ -40,10 +77,13 @@ class ViewController: UIViewController{
         components?.queryItems = [term, media]
         
         guard let url = components?.url else {
+            print("url이 잘못되었습니다")
             return
         }
         var request = URLRequest(url: url)
         request.httpMethod = "GET"
+        
+        
         
        let task = session.dataTask(with: request) { data, response, error in
            
@@ -56,6 +96,9 @@ class ViewController: UIViewController{
                    
                    self.movieModel =  try  JSONDecoder().decode(MovieModel.self , from: hasData)
                    print(self.movieModel ?? "no data")
+                   
+                   self.movieModel?.results.sort{ $0.trackName ?? "" < $1.trackName ?? ""}
+                   
                    DispatchQueue.main.async {
                        self.movieTableView.reloadData()
                    }
@@ -90,11 +133,33 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
         cell.priceLabel.text = currency + price
         
         
+        if let hasURL = self.movieModel?.results[indexPath.row].image {
+                                    //여기서 이미지는 hasㅕ끼을 통해서 가져오는 이미지
+            self.loadImage(urlString: hasURL) { image in
+                cell.movieimageView.image = image
+            }
+        }
+        
+        //dateString = "2016-10-10T07:00:00Z"
+        if let dateString = self.movieModel?.results[indexPath.row].releaseDate {
+        
+        let formatter = ISO8601DateFormatter()
+            if let isoDate = formatter.date(from: dateString){
+                let myFormatter = DateFormatter()
+                    myFormatter.dateFormat = "yyyy년 MM월 dd일"
+                let dateString =  myFormatter.string(from: isoDate)
+                
+                cell.dateLabel.text = dateString
+            }
+        
+        }
         return cell
     }
     
+    
+    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 300
+        return UITableView.automaticDimension
     }
 }
 
@@ -103,4 +168,4 @@ extension ViewController: UISearchBarDelegate{
         
         
     }
-    }
+}

95f4f6584777d0a9f195039128a2ab3520bd824f
YunL97 <leeys9932@naver.com>
2022-01-07 22:24:41 +0900

    save, delete, update 추가

--- UIKit/TodoList/TodoList/TodoDetailViewController.swift
+++ UIKit/TodoList/TodoList/TodoDetailViewController.swift
@@ -30,9 +30,15 @@ class TodoDetailViewController: UIViewController {
     
     @IBOutlet weak var highButton: UIButton!
     
+    @IBOutlet weak var deleteButton: UIButton!
+    
+    @IBOutlet weak var saveButton: UIButton!
     var selectedTodoList: TodoList?
     
     var priority:PriorityLevel?
+    
+    //NSManagedObjectContext
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -42,10 +48,24 @@ class TodoDetailViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
+        //업데이트 일때 (selectedTodoList에 데이터가 있을 떄)
         if let hasdata = selectedTodoList {
             titleTextField.text = hasdata.title
-            print(hasdata.prioirtyLevel)
+            print(hasdata.title!)
+            
+            //셋팅
             priority = PriorityLevel(rawValue: hasdata.prioirtyLevel)
+            makePriorityButtonDesign()
+            
+            deleteButton.isHidden = false
+            saveButton.setTitle("Update", for: .normal)
+            
+            
+        }
+        //새로 만들기 일떄
+        else {
+            deleteButton.isHidden = true
+            saveButton.setTitle("Save", for: .normal)
         }
         
     }
@@ -101,38 +121,134 @@ class TodoDetailViewController: UIViewController {
     
     //데이터 저장
     @IBAction func saveTodo(_ sender: Any) {
-     
-        //NSManagedObjectContext
-        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-        
-            // todoList 테이블에 있는 스칼라 가져오기
-            //NSentityDesciption
-        guard    let entityDesciption = NSEntityDescription.entity(forEntityName: "TodoList", in: context) else {
-            return
+        if selectedTodoList != nil{
+            updateTodo()
+        }else {
+            saveTodo()
         }
         
-        //그 모델 그대로 가져옴
-        guard let object = NSManagedObject(entity: entityDesciption, insertInto: context) as? TodoList else{
-            return
-        }
-        object.title = titleTextField.text
-        object.date = Date()
-        object.uuid = UUID()
+                //함수가 끝날 때 프로토콜함수까지 호출하게 만듬, viewcontroller 에서 didfinishData함수가 정의 되어 있음
+                delegate?.didFinishSaveData()
         
-        //색저장
-        object.prioirtyLevel = priority?.rawValue ?? PriorityLevel.level1.rawValue
         
-        //세이브는 uiapplicationdelegate 에서 가능
-        let appDelegate = (UIApplication.shared.delegate as! AppDelegate )
+                //화면내린다고해서 테이블뷰가 자동으로 갱신되지 않음 그래서 프로토콜로 지정해줘야함
+                self.dismiss(animated: true, completion: nil)
+    }
+    
+    func saveTodo() {
         
-        //테이블에 데이터 저장
-        appDelegate.saveContext()
-        
-        //함수가 끝날 때 프로토콜함수까지 호출하게 만듬
+        // todoList 테이블 가져오기
+        //NSentityDesciption
+    guard    let entityDesciption = NSEntityDescription.entity(forEntityName: "TodoList", in: context) else {
+        return
+    }
+    
+    //그 모델 그대로 가져옴
+    guard let object = NSManagedObject(entity: entityDesciption, insertInto: context) as? TodoList else{
+        return
+    }
+    object.title = titleTextField.text
+    object.date = Date()
+    object.uuid = UUID()
+    
+    //색저장
+    object.prioirtyLevel = priority?.rawValue ?? PriorityLevel.level1.rawValue
+    
+    //세이브는 uiapplicationdelegate 에서 가능
+    let appDelegate = (UIApplication.shared.delegate as! AppDelegate )
+    
+    //테이블에 데이터 저장
+    appDelegate.saveContext()
+    
+        //함수가 끝날 때 프로토콜함수까지 호출하게 만듬, viewcontroller 에서 didfinishData함수가 정의 되어 있음
         delegate?.didFinishSaveData()
-        
-        
+
+
         //화면내린다고해서 테이블뷰가 자동으로 갱신되지 않음 그래서 프로토콜로 지정해줘야함
+        self.dismiss(animated: true, completion: nil)
+        
+    }
+    
+    func updateTodo() {
+        
+        guard let hasData = selectedTodoList else {
+            return
+        }
+        
+        guard let hasUUID = hasData.uuid else {
+            return
+        }
+        
+        // todolist 테이블 데이터 불러올 준비
+        let fetchRequest:NSFetchRequest<TodoList> = TodoList.fetchRequest()
+        
+        //todolist 셀렉된 튜플만 가져오기
+        fetchRequest.predicate = NSPredicate(format: "uuid = %@", hasUUID as CVarArg)
+        
+        //컨텍스트에서 fetchRequest 조건이 걸린거를 가져온다
+        do{
+              let loadeddData = try context.fetch(fetchRequest)
+            loadeddData.first?.title = titleTextField.text
+            loadeddData.first?.date = Date()
+            loadeddData.first?.prioirtyLevel = self.priority?.rawValue ?? PriorityLevel.level1.rawValue
+            
+            //세이브는 uiapplicationdelegate 에서 가능
+            let appDelegate = (UIApplication.shared.delegate as! AppDelegate )
+            
+            //테이블에 데이터 저장
+            appDelegate.saveContext()
+            
+            //함수가 끝날 때 프로토콜함수까지 호출하게 만듬, viewcontroller 에서 didfinishData함수가 정의 되어 있음
+            delegate?.didFinishSaveData()
+
+
+            //화면내린다고해서 테이블뷰가 자동으로 갱신되지 않음 그래서 프로토콜로 지정해줘야함
+            self.dismiss(animated: true, completion: nil)
+            
+        }catch{
+            print(error)
+        }
+       
+        
+    }
+    
+    @IBAction  func deleteTodo(_ sender: UIButton) {
+        
+        
+        guard let hasData = selectedTodoList else {
+            return
+        }
+        
+        guard let hasUUID = hasData.uuid else {
+            return
+        }
+        
+        // todolist 테이블 데이터 불러올 준비
+        let fetchRequest:NSFetchRequest<TodoList> = TodoList.fetchRequest()
+        
+        //todolist 셀렉된 튜플만 가져오기
+        fetchRequest.predicate = NSPredicate(format: "uuid = %@", hasUUID as CVarArg)
+        
+        do{
+            let loadedData = try context.fetch(fetchRequest)
+            
+            if let loadFirstData = loadedData.first {
+                    //메모리 상태에 있는거만 지움 그래서 앱 다시키면 있음
+                    context.delete(loadFirstData)
+                
+                //그래서 앱델리게이트 세이브 해주면 된다
+                let appDelegate = (UIApplication.shared.delegate as! AppDelegate )
+                
+                //테이블에 데이터 저장
+                appDelegate.saveContext()
+            }
+            
+            
+        }catch{
+            print(error)
+        }
+        
+        delegate?.didFinishSaveData()
         self.dismiss(animated: true, completion: nil)
     }
     

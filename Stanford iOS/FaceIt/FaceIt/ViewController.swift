d990465198326426ccfdd2b82e837f10ce4d0cb2
YunL97 <leeys9932@naver.com>
2022-02-04 23:12:21 +0900

    페이스앱 생성

---
+++ Stanford iOS/FaceIt/FaceIt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FaceIt
+//
+//  Created by 이윤식 on 2022/02/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

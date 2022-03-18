3b209e90820411014d7c8812e1513b102f50d813
YunL97 <leeys9932@naver.com>
2022-03-19 02:12:19 +0900

    json model

---
+++ UIKit/httpStudy/httpStudy/jsonModel.swift
@@ -0,0 +1,14 @@
+//
+//  jsonModel.swift
+//  httpStudy
+//
+//  Created by 이윤식 on 2022/03/18.
+//
+
+import Foundation
+
+struct JsonModel:Codable {
+    let id :String?
+    let name:String?
+    let phone:String?
+}
